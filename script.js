5d6431ce8a5c327531d414619fec3f11526ca8e6
Raphel <kbfcgamming@gmail.com>
2026-08-31 22:25:44 +0530

    added the progress bar running

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ const powerbutton=document.getElementById("power-button");
 const window1=document.getElementById("window1");
 const window2=document.getElementById("window2")
 
+const progressBar =document.getElementById("progressBar");
+const progressText =document.getElementById("progressText");
+
 powerbutton.addEventListener("click",function(){
     window1.style.display="none";
     window2.style.display="block";
+
+    let progress = 0;
+    const loading=setInterval(function(){
+        progress++;
+
+        progressBar.style.width=progress +"%";
+        progressText.textContent=progress +"%";
+
+        if(progress>=100){
+            clearInterval(loading);
+        }
+    },50);
 });
